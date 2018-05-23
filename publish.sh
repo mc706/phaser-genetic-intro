e9248d29b7dedadf8f39479ce093938fa4b26d38
Ryan McDevitt <mcdevitt.ryan@gmail.com>
2018-05-23 12:15:48 -0400

    update publish script

--- publish.sh
+++ publish.sh
@@ -1,9 +1,9 @@
-#!bin/bash
-git push origin --delete gh-pages
+#!/bin/bash
 echo "!dist/" >> .gitignore
 npm run build
 git add dist/
 git commit -m "Deploy Gitlab Pages"
+git push origin --delete gh-pages
 git subtree push --prefix dist origin gh-pages
 git reset HEAD~
 git checkout -- .gitignore
